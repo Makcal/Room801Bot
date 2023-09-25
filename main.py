4f416ef51cdf84bf94ab47418fb5569f077a9dd4
Makcal <fominmaxim3721@gmail.com>
2023-09-26 00:24:12 +0300

    Sending has been replaced with replying to deliver messages into the same topic

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def cooking(message: telebot.types.Message):
 def who(message: telebot.types.Message, action: str, order_key: str):
     if re.match(TOMORROW_COMMAND_PATTERN, message.text):
         day = datetime.datetime.now(TZ) + datetime.timedelta(days=1)
-        bot.send_message(message.chat.id, f"{ORDER[order_key][(day - FIRST_DAY).days % 4]} {action} tomorrow.")
+        bot.reply_to(message, f"{ORDER[order_key][(day - FIRST_DAY).days % 4]} {action} tomorrow.")
 
     elif (m := re.match(RANDOM_DAY_COMMAND_PATTERN, message.text)) is not None:
         m: re.Match
@@ -44,14 +44,14 @@ def who(message: telebot.types.Message, action: str, order_key: str):
             m = re.match(RANDOM_DAY_COMMAND_PATTERN, message.text)
             day = datetime.datetime.strptime(m.group(1), r"%d.%m.%Y").astimezone(TZ)
         except ValueError:
-            bot.send_message(message.chat.id, "Invalid date")
+            bot.reply_to(message, "Invalid date")
         else:
             day += datetime.timedelta(hours=12)
-            bot.send_message(message.chat.id, f"{ORDER[order_key][(day - FIRST_DAY).days % 4]} {action} that day.")
+            bot.reply_to(message, f"{ORDER[order_key][(day - FIRST_DAY).days % 4]} {action} that day.")
 
     else:
         day = datetime.datetime.now(TZ)
-        bot.send_message(message.chat.id, f"{ORDER[order_key][(day - FIRST_DAY).days % 4]} {action} today.")
+        bot.reply_to(message, f"{ORDER[order_key][(day - FIRST_DAY).days % 4]} {action} today.")
 
 
 bot.infinity_polling()
